2172889e9fa81d62f8849167b3471b2351df534f
Tulio Goncalves <tulio-goncalves@uiowa.edu>
2020-01-08 12:50:47 -0600

    Add project files.

---
+++ EQAPOFE/frmEQAPO.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace EQAPOFE
+{
+    partial class frmEQAPO
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmEQAPO));
+            this.sliderGain = new System.Windows.Forms.TrackBar();
+            this.sliderBand1 = new System.Windows.Forms.TrackBar();
+            this.lblPreamp = new System.Windows.Forms.Label();
+            this.lblBandGain1 = new System.Windows.Forms.Label();
+            this.lblBandHz1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.sliderGain)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sliderBand1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sliderGain
+            // 
+            this.sliderGain.LargeChange = 10;
+            this.sliderGain.Location = new System.Drawing.Point(12, 33);
+            this.sliderGain.Maximum = 120;
+            this.sliderGain.Minimum = -120;
+            this.sliderGain.Name = "sliderGain";
+            this.sliderGain.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.sliderGain.Size = new System.Drawing.Size(45, 250);
+            this.sliderGain.TabIndex = 0;
+            this.sliderGain.TickFrequency = 10;
+            this.sliderGain.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.sliderGain.Scroll += new System.EventHandler(this.sliderGain_Scroll);
+            // 
+            // sliderBand1
+            // 
+            this.sliderBand1.LargeChange = 10;
+            this.sliderBand1.Location = new System.Drawing.Point(90, 33);
+            this.sliderBand1.Maximum = 120;
+            this.sliderBand1.Minimum = -120;
+            this.sliderBand1.Name = "sliderBand1";
+            this.sliderBand1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.sliderBand1.Size = new System.Drawing.Size(45, 250);
+            this.sliderBand1.TabIndex = 0;
+            this.sliderBand1.TickFrequency = 10;
+            this.sliderBand1.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.sliderBand1.Scroll += new System.EventHandler(this.Slider_Scroll);
+            // 
+            // lblPreamp
+            // 
+            this.lblPreamp.AutoSize = true;
+            this.lblPreamp.Location = new System.Drawing.Point(9, 286);
+            this.lblPreamp.Name = "lblPreamp";
+            this.lblPreamp.Size = new System.Drawing.Size(35, 13);
+            this.lblPreamp.TabIndex = 1;
+            this.lblPreamp.Text = "label1";
+            // 
+            // lblBandGain1
+            // 
+            this.lblBandGain1.AutoSize = true;
+            this.lblBandGain1.Location = new System.Drawing.Point(87, 286);
+            this.lblBandGain1.Name = "lblBandGain1";
+            this.lblBandGain1.Size = new System.Drawing.Size(35, 13);
+            this.lblBandGain1.TabIndex = 1;
+            this.lblBandGain1.Text = "label1";
+            // 
+            // lblBandHz1
+            // 
+            this.lblBandHz1.AutoSize = true;
+            this.lblBandHz1.Location = new System.Drawing.Point(87, 17);
+            this.lblBandHz1.Name = "lblBandHz1";
+            this.lblBandHz1.Size = new System.Drawing.Size(35, 13);
+            this.lblBandHz1.TabIndex = 2;
+            this.lblBandHz1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Preamp";
+            // 
+            // frmEQAPO
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(147, 309);
+            this.Controls.Add(this.lblBandHz1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblBandGain1);
+            this.Controls.Add(this.lblPreamp);
+            this.Controls.Add(this.sliderBand1);
+            this.Controls.Add(this.sliderGain);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmEQAPO";
+            this.Text = "Equalizer APO";
+            this.Load += new System.EventHandler(this.frmEQAPO_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.sliderGain)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sliderBand1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar sliderGain;
+        private System.Windows.Forms.TrackBar sliderBand1;
+        private System.Windows.Forms.Label lblPreamp;
+        private System.Windows.Forms.Label lblBandGain1;
+        private System.Windows.Forms.Label lblBandHz1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ EQAPOFE/frmEQAPO.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EQAPOFE
+{
+    public partial class frmEQAPO : Form
+    {
+        List<string> eqLines = new List<string>();
+        string[] eqItems;
+
+#if DEBUG
+        string configPath = $@"C:\Program Files\EqualizerAPO\config\config.txt";
+#else
+        string configPath = $@"{Application.StartupPath}\config.txt";
+#endif
+
+        public frmEQAPO()
+        {
+            InitializeComponent();
+        }
+
+        private void frmEQAPO_Load(object sender, EventArgs e)
+        {
+            if (!File.Exists(configPath))
+            {
+                MessageBox.Show("Cannot find configuration file. Please place this program on the same folder as config.txt.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+
+            using (StreamReader eqFile = new StreamReader(File.OpenRead(configPath)))
+            {
+                while (!eqFile.EndOfStream)
+                {
+                    eqLines.Add(eqFile.ReadLine());
+                }
+            }
+
+            string gainLine = eqLines.Where(c => c.StartsWith("Preamp:")).FirstOrDefault();
+            sliderGain.Value = Convert.ToInt32(gainLine.Split(' ')[1]) * 10;
+            lblPreamp.Text = $"{gainLine.Split(' ')[1]} dB";
+
+
+            string graphEqLine = eqLines.Where(c => c.StartsWith("GraphicEQ:")).FirstOrDefault();
+
+            eqItems = graphEqLine.Substring(graphEqLine.IndexOf(":") + 1).Split(';');
+            for (int i = 0; i < eqItems.Length; i++)
+            {
+                string[] bandData = eqItems[i].Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                if (i > 0)
+                {
+                    TrackBar newSlider = new TrackBar()
+                    {
+                        Name = $"sliderBand{i + 1}",
+                        Maximum = sliderBand1.Maximum,
+                        Minimum = sliderBand1.Minimum,
+                        TickFrequency = sliderBand1.TickFrequency,
+                        Orientation = sliderBand1.Orientation,
+                        TickStyle = sliderBand1.TickStyle,
+                        Height = sliderBand1.Height,
+                        Top = sliderBand1.Top,
+                        Left = sliderBand1.Left + (sliderBand1.Width * i),
+                        Visible = true
+                    };
+                    Label newLblHz = new Label()
+                    {
+                        Name = $"lblBandHz{i + 1}",
+                        AutoSize = true,
+                        Size = lblBandHz1.Size,
+                        Top = lblBandHz1.Top,
+                        Left = newSlider.Left,
+                        Visible = true
+                    };
+                    Label newLblGain = new Label()
+                    {
+                        Name = $"lblBandGain{i + 1}",
+                        AutoSize = true,
+                        Size = lblBandGain1.Size,
+                        Top = lblBandGain1.Top,
+                        Left = newSlider.Left,
+                        Visible = true
+                    };
+                    this.Width += sliderBand1.Width;
+
+                    newSlider.Scroll += Slider_Scroll;
+
+                    this.Controls.Add(newSlider);
+                    this.Controls.Add(newLblHz);
+                    this.Controls.Add(newLblGain);
+                }
+
+                TrackBar slider = this.Controls.Find($"sliderBand{i + 1}", true)[0] as TrackBar;
+                Label lblHz = this.Controls.Find($"lblBandHz{i + 1}", true)[0] as Label;
+                Label lblGain = this.Controls.Find($"lblBandGain{i + 1}", true)[0] as Label;
+
+                slider.Value = (int)(Convert.ToDouble(bandData[1]) * 10);
+                lblHz.Text = (Convert.ToDouble(bandData[0]) >= 1000 ? $"{Convert.ToDouble(bandData[0]) / 1000.0} KHz" : $"{bandData[0]} Hz");
+                lblGain.Text = $"{bandData[1]} dB";
+
+            }
+
+        }
+
+        private void Slider_Scroll(object sender, EventArgs e)
+        {
+            TrackBar sliderBand = sender as TrackBar;
+            int sliderNumber = Convert.ToInt32(sliderBand.Name.Replace("sliderBand", ""));
+            Label lblGain = this.Controls.Find($"lblBandGain{sliderNumber}", true)[0] as Label;
+
+            lblGain.Text = $"{sliderBand.Value / 10.0} dB";
+            string[] bandData = eqItems[sliderNumber - 1].Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            bandData[1] = (sliderBand.Value / 10.0).ToString();
+            eqItems[sliderNumber - 1] = string.Join(" ", bandData);
+
+            StringBuilder configText = new StringBuilder();
+            for (int i =0; i<eqLines.Count;i++)
+            {
+                if (eqLines[i].StartsWith("GraphicEQ:"))
+                {
+                    eqLines[i] = $"GraphicEQ: {string.Join("; ", eqItems)}";
+                }
+                configText.AppendLine(eqLines[i]);
+            }
+
+            try
+            {
+                File.WriteAllText(configPath, configText.ToString());
+            }
+            catch { }
+        }
+
+        private void sliderGain_Scroll(object sender, EventArgs e)
+        {
+            lblPreamp.Text = $"{sliderGain.Value / 10.0} dB";
+
+            StringBuilder configText = new StringBuilder();
+            for (int i = 0; i < eqLines.Count; i++)
+            {
+                if (eqLines[i].StartsWith("Preamp:"))
+                {
+                    eqLines[i] = $"Preamp: {sliderGain.Value / 10.0} dB";
+                }
+                configText.AppendLine(eqLines[i]);
+            }
+
+            try
+            {
+                File.WriteAllText(configPath, configText.ToString());
+            }
+            catch { }
+
+        }
+    }
+}
